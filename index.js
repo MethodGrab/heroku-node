568c71eaeb95ca21f409d0d76cc5630b30852ded
MethodGrab <methodgr4b@gmail.com>
2016-01-15 14:00:50 +0000

    Decaffeinate

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-'use strict';
+const express = require( 'express' );
+const morgan  = require( 'morgan' );
 
-// bootstrap CoffeeScript
-require( 'coffee-script/register' );
-module.exports = require( './index.coffee' );
+const PORT    = process.env.PORT || 3000;
+const ENV     = process.env.ENV || 'development';
+const app     = express();
+
+app.use( morgan( ENV === 'development' ? 'dev' : 'combined' ) );
+app.use( express.static( 'public' ) );
+
+const server = app.listen( PORT, () => {
+	console.log( `Express app is listening on http://localhost:${PORT}` );
+});
